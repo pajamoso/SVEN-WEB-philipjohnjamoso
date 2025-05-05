5ce713b48a1340b96544af2d2f780c714a74bab2
philip.jamoso <philip.jamoso@global-imi.com>
2025-05-05 14:10:43 +0800

    Deleting redundant folder

--- pawtastic-pet-services/backend/src/main/java/com/pawtastic/petservices/petservices/src/main/java/com/pawtastic/petservices/repository/FormRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pawtastic.petservices.repository;
-
-import com.pawtastic.petservices.entity.FormRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormRequestRepository extends JpaRepository<FormRequest, Long> {
-}
